abbf8730c492ce013eeba6ff6ef9f452cd02e53a
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2022-08-06 22:13:26 -0400

    Update bytes.sh

--- bytes.sh
+++ bytes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Need to rewrite this.
-# Please hit CTRL+C to close when the numbers stop going up.
+
 for (( ;; ))
 do
         clear && wc -c pi.txt | sed "s/p.*/bytes/g" && sleep 1
